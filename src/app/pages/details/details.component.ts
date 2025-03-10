9eb5b5b04b4e283202b59131600a254f90852e8d
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-03-10 18:40:05 -0300

    corrigindo o details component, injeção de dependencia feita da forma tradicional

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -10,30 +10,39 @@ import { forkJoin } from 'rxjs';
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
-export class DetailsComponent implements OnInit{
+export class DetailsComponent implements OnInit {
 
   // Injeções de dependência
-   private activatedRoute = Inject(ActivatedRoute);
-   private pokeApiService = Inject(PokeApiService);
+  //  private activatedRoute = Inject(ActivatedRoute);
+  //  private pokeApiService = Inject(PokeApiService);
 
   // urls advindas da API
-   private urlPokemon = 'https://pokeapi.co./api/v2/pokemon';
-  private urlName = 'https://pokeapi.co./api/v2/pokemon-species';
+  private urlPokemon = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  // a injeção precisa ser feita no construtor pra funcionar 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.pokemon;
   }
 
-  get pokemon(){
-    const id = this.activatedRoute.snapshot.params['id'];
-    const pokemon = this.pokeApiService.apiGetPokemon(this.urlPokemon + '/id');
-    const name = this.pokeApiService.apiGetPokemon(this.urlName + '/id');
-
-   return forkJoin([pokemon, name]).subscribe(
-      res => {
-        console.log(res);
+    get pokemon() {
+      const id = this.activatedRoute.snapshot.paramMap.get('id');
+      if (!id) {
+        console.error('ID não encontrado na rota');
+        return;
       }
-    );
+  
+      const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
+      const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+  
+      forkJoin([pokemon, name]).subscribe(
+        res => console.log(res),
+        err => console.error('Erro ao buscar Pokémon:', err)
+      );
+    }
   }
-
-}
